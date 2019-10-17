a5d3e760b6834f144278d35d448579cfa40fb5cd
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-17 01:27:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,9 +7,10 @@ def oxford_comma(array)
 elsif array.length == 2
     return "#{array[0]} and #{array[1]}"
   else
-    element = array[-1].insert(0, "and ")
-    array.unshift << element
-    array.uniq.join(", ")
+    #element = array[-1].insert(0, "and ")
+    #array.unshift << element
+    #array.uniq.join(", ")
+    
 end
 
 #binding.pry
