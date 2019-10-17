d70aadcb6eadc30d04bc6862006dbce6ec7969dc
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-17 00:53:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
 elsif array.length == 2
     return "#{array[0]} and #{array[1]}"
   else
-    return array.insert(-2, "and").join(", ")
+    return array.join(", ").insert(-2, "and")
   end
 
 #binding.pry
